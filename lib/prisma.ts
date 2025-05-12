e7c76905fd0536d1e6ffbc16059ecc8269f525b1
Amit Patel <154122255+AmitwithAPJ@users.noreply.github.com>
2025-05-12 22:33:15 +0530

    first commit

---
+++ lib/prisma.ts
@@ -0,0 +1,15 @@
+// lib/prisma.ts
+
+import { PrismaClient } from '../app/generated/prisma'; // or use path alias if configured
+
+
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
+
+export const prisma =
+  globalForPrisma.prisma ?? new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
